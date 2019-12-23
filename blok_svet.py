25133580fc66c69c867f3c5a341bea26708bb272
Luka <lslukasavic9@gmail.com>
2019-12-23 04:49:02 +0100

    Q vrednost iteracije radi...isprobano za GAMMA = 1 i 0.9

--- blok_svet.py
+++ blok_svet.py
@@ -4,77 +4,121 @@ import matplotlib.pyplot as plt
 import constants as con
 
 
+def policy(S):
+    return S.max_Q().a
+
+
+def next_state(state, dir):
+    x, y = state.S[0], state.S[1]
+    if dir == con.UP and x - 1 >= 0 and (x - 1, y) != con.WALL:
+        return board[x - 1][y]
+    elif dir == con.DOWN and x + 1 < con.ROWS and (x + 1, y) != con.WALL:
+        return board[x + 1][y]
+    elif dir == con.LEFT and y - 1 >= 0 and (x, y - 1) != con.WALL:
+        return board[x][y - 1]
+    elif dir == con.RIGHT and y + 1 < con.COLS and (x, y + 1) != con.WALL:
+        return board[x][y + 1]
+    else:
+        return board[x][y]
+
+
+def transform(dir):
+    if dir == con.UP:
+        return 'UP'
+    elif dir == con.DOWN:
+        return 'DOWN'
+    elif dir == con.LEFT:
+        return 'LEFT'
+    else:
+        return 'RIGHT'
+
+
 def main():
-    print('a')
-    for i in range(con.ROWS):
-        row = []
-        for j in range(con.COLS):
-            if (i, j) == (0, 3):
-                row.append(State((i, j), 1))
-            elif (i, j) == (1, 3):
-                row.append(State((i, j), -1))
-            elif (i, j) != (1, 1):
-                row.append(State((i, j), -0.04))
-            else:
-                row.append(None)
-        board.append(row)
-    for iter in range(100):
-        print('--------------------------------------------------------')
-        for i in range(len(board)):
-            print()
-            for j in range(len(board[0])):
-                if (i, j) != con.WALL and (i, j) != con.GOAL and (i, j) != con.FAIL:
-                    board[i][j].calc_Q()
-                    print(i, j, board[i][j].Q_next[0].val, board[i][j].Q_next[1].val, board[i][j].Q_next[2].val,
-                          board[i][j].Q_next[3].val)
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if (i, j) != con.WALL and (i, j) != con.GOAL and (i, j) != con.FAIL:
-                    board[i][j].next_Q()
-    for i in range(len(board)):
-        print('--------------------------------------')
-        for j in range(len(board[0])):
-            # S = board[i][j]
-            # print(board)
-            for k in range(4):
-                if board[i][j] is not None:
-                    print(i, j, 'Q', k, board[i][j].Q[k], board[i][j].R)
+    for g in con.GAMMA:
+        print('GAMMA: ', g, '-------------------------')
+        board.clear()
+        for i in range(con.ROWS):
+            row = []
+            for j in range(con.COLS):
+                if (i, j) == con.GOAL:
+                    row.append(State((i, j), 1, g))
+                elif (i, j) == con.FAIL:
+                    row.append(State((i, j), -1, g))
+                elif (i, j) != con.WALL:
+                    row.append(State((i, j), -0.04, g))
+                else:
+                    row.append(None)
+            board.append(row)
+        for iter in range(50):
+            for i in range(len(board)):
+                if iter == 49:
+                    print()
+                for j in range(len(board[0])):
+                    if (i, j) != con.WALL and (i, j) != con.GOAL and (i, j) != con.FAIL:
+                        board[i][j].calc_Q()
+                        if iter == 49:
+                            print(i, j, board[i][j].Q[0].next_val, board[i][j].Q[1].next_val, board[i][j].Q[2].next_val,
+                                  board[i][j].Q[3].next_val)
+            for i in range(len(board)):
+                for j in range(len(board[0])):
+                    if (i, j) != con.WALL and (i, j) != con.GOAL and (i, j) != con.FAIL:
+                        board[i][j].next_Q()
+
+        S_curr = board[2][0]
+        while S_curr.S != con.GOAL and S_curr.S != con.FAIL:
+            a = policy(S_curr)
+            S_curr = next_state(S_curr, a)
+            print(transform(a), S_curr.S)
+
+    # print('###################################')
+    # for i in range(len(board)):
+    #     print()
+    #     for j in range(len(board[0])):
+    #         # S = board[i][j]
+    #         # print(board)
+    #         # for k in range(4):
+    #         if board[i][j] is not None:
+    #             print(i, j, 'Q', board[i][j].max_Q().val)
 
 
 class State:
     # S = state (x, y)
-    def __init__(self, S, R):
+    def __init__(self, S, R, gamma):
         self.S = S
         self.R = R
-        self.Q = [Q(self, count) for count in con.directions]
-        self.Q_next = self.Q
+        self.gamma = gamma
+        self.Q = [Q(self, count, self.R) for count in con.directions]
 
     def next_Q(self):
-        self.Q = self.Q_next
+        Q = self.Q
+        for i in range(len(Q)):
+            Q[i].val = Q[i].next_val
 
     def calc_Q(self):
-        for q in self.Q_next: q.next_val()
+        for q in self.Q: q.calc_next_val()
 
     def max_Q(self):
         max_q = self.Q[0]
         for q in self.Q:
             if max_q.val < q.val:
                 max_q = q
-        return max_q.val
+        return max_q
 
 
 class Q:
     # a = direction: UP, DOWN, LEFT, RIGHT
-    def __init__(self, state, a):
+    def __init__(self, state, a, R):
         self.state = state
         self.a = a
-        self.val = 0
+        self.val = R
+        self.next_val = R
 
-    def next_val(self):
+    def calc_next_val(self):
         sum = 0
         for dir in con.directions:
-            sum += self.p(dir) * self.next_state(dir).max_Q()
-        self.val = self.state.R + con.GAMMA * sum
+            sum += self.p(dir) * next_state(self.state, dir).max_Q().val
+        self.next_val = self.state.R + self.state.gamma * sum
+        self.next_val
 
     # directions = [UP, DOWN, LEFT, RIGHT]
     def p(self, dir):
@@ -84,19 +128,6 @@ class Q:
             return 0
         else:
             return 0.1
-
-    def next_state(self, dir):
-        x, y = self.state.S[0], self.state.S[1]
-        if dir == con.UP and x - 1 > 0 and (x - 1, y) != con.WALL:
-            return board[x - 1][y]
-        elif dir == con.DOWN and x + 1 < con.ROWS and (x + 1, y) != con.WALL:
-            return board[x + 1][y]
-        elif dir == con.LEFT and y - 1 > 0 and (x, y - 1) != con.WALL:
-            return board[x][y - 1]
-        elif dir == con.RIGHT and y + 1 < con.COLS and (x, y + 1) != con.WALL:
-            return board[x][y + 1]
-        else:
-            return board[x][y]
 
 
 if __name__ == "__main__":
--- venv/constants.py
+++ venv/constants.py
@@ -3,4 +3,5 @@ directions = [UP, DOWN, LEFT, RIGHT]
 opposite = [DOWN, UP, RIGHT, LEFT]
 ROWS, COLS = 3, 4
 GOAL, FAIL, WALL, START = (0, 3), (1, 3), (1, 1), (2, 0)
-GAMMA = 1
+GAMMA = [1, 0.9]
+POS, NEG, NEUT = 1, [-1, -100], -0.04
